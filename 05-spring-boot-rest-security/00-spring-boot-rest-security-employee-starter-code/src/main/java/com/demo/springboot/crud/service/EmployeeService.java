24f36c4d142108d5a3088680ae47e151b2ad82ad
percy <sunpeng.fhc@gmail.com>
2026-04-22 17:18:34 -0600

    feat: practice Spring Security with role-based authorization

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/demo/springboot/crud/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.demo.springboot.crud.service;
+
+import com.demo.springboot.crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/test/java/com/demo/springboot/crud/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springboot.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
