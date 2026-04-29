29fceaca30b0dcb2a0d34aad43f2d4bed7c0a232
percy <sunpeng.fhc@gmail.com>
2026-04-29 17:33:27 -0600

    feat: practice JDBC authentication with custom user and role queries

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/demo/springboot/crud/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/demo/springboot/crud/security/DemoSecurityConfig.java
@@ -4,70 +4,87 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
+        // define query to retrieve a suer by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id = ?");
 
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,mary,susan);
-
-    }
-
-
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                    .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
-                    .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
-                    .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
-
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "SELECT user_id, role FROM  roles where user_id = ?"
         );
 
-        // use HTTP Basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // disable Cross Site Request Forgery (CSRF)
-        // in general , not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
+        return new JdbcUserDetailsManager(dataSource);
     }
 
+
+
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                    .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+//                    .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+//                    .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+//                    .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+//                    .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
+//                    .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+//
+//        );
+//
+//        // use HTTP Basic authentication
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // disable Cross Site Request Forgery (CSRF)
+//        // in general , not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+//        http.csrf(csrf -> csrf.disable());
+//
+//        return http.build();
+//    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,mary,susan);
+//
+//    }
 
 
 
